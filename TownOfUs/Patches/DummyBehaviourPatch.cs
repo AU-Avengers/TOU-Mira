c3a75dca8c978bdf6a153e459876e6b9c4be5d06
xChipseq <sztabekminecraft@gmail.com>
2025-08-05 19:34:01 +0200

    removed the dummy vote patches, they are now in mira

--- TownOfUs/Patches/DummyBehaviourPatch.cs
+++ TownOfUs/Patches/DummyBehaviourPatch.cs
@@ -2,12 +2,9 @@ using System.Collections;
 using HarmonyLib;
 using MiraAPI.Modifiers;
 using MiraAPI.Roles;
-using MiraAPI.Utilities;
-using MiraAPI.Voting;
 using Reactor.Utilities;
 using Reactor.Utilities.Extensions;
 using TownOfUs.Modifiers.Game;
-using TownOfUs.Roles.Crewmate;
 using TownOfUs.Roles.Neutral;
 using TownOfUs.Utilities;
 using UnityEngine;
@@ -24,85 +21,6 @@ public static class DummyBehaviourPatches
     {
         var dum = __instance.myPlayer;
         Coroutines.Start(TouDummyMode(dum));
-    }
-    
-    [HarmonyPrefix]
-    [HarmonyPatch(nameof(DummyBehaviour.Update))]
-    public static bool DummyUpdatePatch(DummyBehaviour __instance)
-    {
-        NetworkedPlayerInfo data = __instance.myPlayer.Data;
-        if (data == null || data.IsDead) return false;
-        
-        if (MeetingHud.Instance)
-        {
-            // My attempts in making them vote correctly failed due to how the tribunal voting is handled,
-            // so I made them vote with the player
-            if (MarshalRole.TribunalHappening)
-            {
-                var localVoteData = PlayerControl.LocalPlayer.GetVoteData();
-                if (localVoteData.Votes.Count == 0) return false;
-                if (DidVote(__instance)) return false;
-                
-                Coroutines.Start(CoDelayedVote(
-                    __instance,
-                    localVoteData.Votes[0].Suspect,
-                    new FloatRange(0.5f, 2f)));
-                
-                return false;
-            }
-            
-            if (!DidVote(__instance))
-            {
-                // This was made to work with tribunals, now it's just a better version of the vanilla method lol
-                CustomDoVote(__instance);
-            }
-        }
-        
-        return false;
-    }
-
-    private static void CustomDoVote(DummyBehaviour dummy)
-    {
-        if (DidVote(dummy)) return;
-        if (MarshalRole.TribunalHappening) return;
-        
-        List<byte> potentialSuspects = new();
-        potentialSuspects.AddRange(Helpers.GetAlivePlayers()
-            .Where(p => p != dummy.myPlayer)
-            .Select(p => p.PlayerId));
-
-        if (CanSkip(dummy))
-        {
-            potentialSuspects.Add(253); // the skip button
-        }
-
-        if (dummy.myPlayer.GetVoteData().VotesRemaining > 0)
-        {
-            Coroutines.Start(CoDelayedVote(dummy, potentialSuspects.Random(), dummy.voteTime));
-        }
-    }
-
-    private static IEnumerator CoDelayedVote(DummyBehaviour dummy, byte suspect, FloatRange range)
-    {
-        dummy.voted = true;
-        yield return new WaitForSeconds(range.Next());
-        VotingUtils.RpcCastVote(PlayerControl.LocalPlayer, dummy.myPlayer.PlayerId, suspect);
-    }
-
-    private static bool DidVote(DummyBehaviour dummy)
-    {
-        // If dummy.voted is true, but they have no votes (tribunal scenario), reset it
-        if (dummy.voted && dummy.myPlayer.GetVoteData().Votes.Count <= 0)
-            dummy.voted = false;
-        
-        // During a tribunal, technically the player did NOT vote
-        // That's why we check for any votes in the list and dummy.voted just in case
-        return dummy.myPlayer.GetVoteData().Votes.Count > 0 || dummy.voted;
-    }
-
-    private static bool CanSkip(DummyBehaviour dummy)
-    {
-        return !MarshalRole.TribunalHappening;
     }
 
     private static IEnumerator TouDummyMode(PlayerControl dummy)
