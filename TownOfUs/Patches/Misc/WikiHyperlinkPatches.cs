823c71229b3fdab0126d1d465fa4501896fd9c34
Atony <75191600+AtonyGit@users.noreply.github.com>
2025-09-14 14:23:52 -0500

    Fix Impostor Ghost role error

--- TownOfUs/Patches/Misc/WikiHyperlinkPatches.cs
+++ TownOfUs/Patches/Misc/WikiHyperlinkPatches.cs
@@ -53,7 +53,7 @@ public static class WikiHyperLinkPatches
                 else
                 {
                     // Non-custom roles (aka vanilla ones) can also be tagged, but they have no wiki entries.
-                    role = RoleManager.Instance.AllRoles.FirstOrDefault(x => x.GetRoleName().Equals(key, StringComparison.OrdinalIgnoreCase));
+                    role = RoleManager.Instance.AllRoles.ToArray().FirstOrDefault(x => x.GetRoleName().Equals(key, StringComparison.OrdinalIgnoreCase));
                     if (role != null)
                     {
                         replacement = $"{fontTag}<b>{role.TeamColor.ToTextColor()}{role.GetRoleName()}</color></b></font>";
