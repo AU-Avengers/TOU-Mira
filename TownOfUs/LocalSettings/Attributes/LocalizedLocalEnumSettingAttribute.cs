f4ba8753adf0b6d9b6ae2e8f13cba3b47ac83928
Atony <75191600+AtonyGit@users.noreply.github.com>
2025-09-20 23:48:39 -0500

    Add new options menu

---
+++ TownOfUs/LocalSettings/Attributes/LocalizedLocalEnumSettingAttribute.cs
@@ -0,0 +1,27 @@
+﻿using BepInEx.Configuration;
+using MiraAPI.LocalSettings.Attributes;
+using TownOfUs.LocalSettings.SettingTypes;
+
+namespace TownOfUs.LocalSettings.Attributes;
+
+/// <summary>
+/// Creates an enum setting for the <see cref="ConfigEntry{T}"/>.
+/// </summary>
+/// <param name="names">Optional custom enum names.</param>
+/// <inheritdoc/>
+[AttributeUsage(AttributeTargets.Property)]
+public class LocalizedLocalEnumSettingAttribute(
+    string? name = null,
+    string? description = null,
+    string[]? names = null
+    ) : LocalEnumSettingAttribute(name, description)
+{
+    private readonly string? _name = name;
+    private readonly string? _description = description;
+
+    /// <inheritdoc/>
+    public override LocalizedLocalEnumSetting CreateSetting(Type tab, ConfigEntryBase configEntryBase)
+    {
+        return new LocalizedLocalEnumSetting(tab, configEntryBase, configEntryBase.SettingType, _name, _description, names);
+    }
+}
---
+++ TownOfUs/LocalSettings/Attributes/LocalizedLocalSliderSettingAttribute.cs
@@ -0,0 +1,36 @@
+﻿using BepInEx.Configuration;
+using MiraAPI.LocalSettings.Attributes;
+using MiraAPI.Utilities;
+using TownOfUs.LocalSettings.SettingTypes;
+
+namespace TownOfUs.LocalSettings.Attributes;
+
+/// <summary>
+/// Creates a slider setting for the <see cref="ConfigEntry{T}"/>.
+/// </summary>
+/// <inheritdoc/>
+/// <param name="min">Minimum range.</param>
+/// <param name="max">Maximum range.</param>
+/// <param name="roundValue">Should the value be rounded.</param>
+/// <param name="suffixType">Suffix for the value.</param>
+[AttributeUsage(AttributeTargets.Property)]
+public class LocalizedLocalSliderSettingAttribute(
+    string? name = null,
+    float min = 0,
+    float max = 100,
+    string? description = null,
+    bool displayValue = false,
+    string? formatString = null,
+    bool roundValue = false,
+    MiraNumberSuffixes suffixType = MiraNumberSuffixes.None
+    ) : LocalSliderSettingAttribute(name, description:description)
+{
+    private readonly string? _name = name;
+    private readonly string? _description = description;
+
+    /// <inheritdoc/>
+    public override LocalizedLocalSliderSetting CreateSetting(Type tab, ConfigEntryBase configEntryBase)
+    {
+        return new LocalizedLocalSliderSetting(tab, configEntryBase, _name, _description, new FloatRange(min, max), displayValue, suffixType, formatString, roundValue);
+    }
+}
