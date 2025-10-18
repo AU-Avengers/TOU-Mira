c0d7107636ced4bb59a3f9f1c1120fbf118a7777
Atony <75191600+AtonyGit@users.noreply.github.com>
2025-10-18 13:56:18 -0500

    Fix skeld polus doors properly

--- TownOfUs/Patches/PrefabChanging/SkeldDoorPatch.cs
+++ TownOfUs/Patches/PrefabChanging/SkeldDoorPatch.cs
@@ -1,15 +1,18 @@
 using HarmonyLib;
 using MiraAPI.GameOptions;
+using PowerTools;
+using Reactor.Utilities.Extensions;
 using TownOfUs.Options.Maps;
+using UnityEngine;
 
 namespace TownOfUs.Patches.PrefabChanging;
 
 [HarmonyPatch]
 public static class SkeldDoorPatch
 {
-    [HarmonyPatch(typeof(SkeldShipStatus), nameof(SkeldShipStatus.OnEnable))]
+    [HarmonyPatch(typeof(ShipStatus), nameof(ShipStatus.OnEnable))]
     [HarmonyPostfix]
-    public static void Postfix(SkeldShipStatus __instance)
+    public static void Postfix(ShipStatus __instance)
     {
         if (!OptionGroupSingleton<BetterSkeldOptions>.Instance.SkeldPolusDoors)
         {
@@ -17,9 +20,25 @@ public static class SkeldDoorPatch
         }
 
         var polusdoor = PrefabLoader.Polus.GetComponentInChildren<DoorConsole>().MinigamePrefab;
-        foreach (var door in __instance.GetComponentsInChildren<DoorConsole>())
+        var doors = __instance.GetComponentsInChildren<AutoOpenDoor>().Select(x => x.gameObject).ToArray();
+        foreach (var door in doors)
         {
-            door.MinigamePrefab = polusdoor;
+            var autoDoor = door.GetComponent<AutoOpenDoor>();
+            var consoleDoor = door.AddComponent<DoorConsole>();
+            var plainDoor = door.AddComponent<PlainDoor>();
+            plainDoor.animator = door.GetComponent<SpriteAnim>();
+            plainDoor.CloseDoorAnim = autoDoor.CloseDoorAnim;
+            plainDoor.CloseSound = autoDoor.CloseSound;
+            plainDoor.myCollider = autoDoor.myCollider;
+            plainDoor.OpenDoorAnim = autoDoor.OpenDoorAnim;
+            plainDoor.OpenSound = autoDoor.OpenSound;
+            plainDoor.shadowCollider = autoDoor.shadowCollider;
+            plainDoor.size = autoDoor.size;
+            plainDoor.Open = autoDoor.Open;
+            consoleDoor.Image = door.GetComponent<SpriteRenderer>();
+            consoleDoor.MyDoor = plainDoor;
+            consoleDoor.MinigamePrefab = polusdoor;
+            autoDoor.Destroy();
         }
     }
 }
