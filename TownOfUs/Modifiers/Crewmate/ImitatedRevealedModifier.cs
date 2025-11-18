7357e5478d7f9cae88f18cfc83882ec392435517
Atony <75191600+AtonyGit@users.noreply.github.com>
2025-11-17 19:00:10 -0600

    Fix imitator revealing non-crewmate roles

--- TownOfUs/Modifiers/Crewmate/ImitatedRevealedModifier.cs
+++ TownOfUs/Modifiers/Crewmate/ImitatedRevealedModifier.cs
@@ -23,9 +23,15 @@ public sealed class ImitatedRevealedModifier(RoleBehaviour role)
     }
 
     // TODO: Fix Imitator not showing role icons on already imitated players. Is this required? No, but it makes it more visually appealing.
-    /*public override void OnMeetingStart()
+    public override void OnMeetingStart()
     {
-        if (ShownRole == null)
+        var roleWhenAlive = Player.GetRoleWhenAlive();
+        if (roleWhenAlive is ICrewVariant crewType)
+        {
+            roleWhenAlive = crewType.CrewVariant;
+        }
+        SetNewInfo(true, null, null, roleWhenAlive);
+        /*if (ShownRole == null)
         {
             return;
         }
@@ -54,6 +60,6 @@ public sealed class ImitatedRevealedModifier(RoleBehaviour role)
 
                 break;
             }
-        }
-    }*/
+        }*/
+    }
 }
--- TownOfUs/Options/Roles/Crewmate/AltruistOptions.cs
+++ TownOfUs/Options/Roles/Crewmate/AltruistOptions.cs
@@ -9,18 +9,9 @@ public sealed class AltruistOptions : AbstractOptionGroup<AltruistRole>
 {
     public override string GroupName => TouLocale.Get("TouRoleAltruist", "Altruist");
 
-    public ModdedEnumOption KIllersAlertedAtStart { get; } =
-        new("Killers Alerted Before Revive", (int)InformedKillers.Nobody, typeof(InformedKillers), ["Nobody", "Neutral Killers", "Impostors", "Neutrals and Impostors"]);
-
-    public ModdedEnumOption KIllersAlertedAtEnd { get; } =
-        new("Killers Alerted After Revive", (int)InformedKillers.NeutralsAndImpostors, typeof(InformedKillers), ["Nobody", "Neutral Killers", "Impostors", "Neutrals and Impostors"]);
-    
     public ModdedEnumOption ReviveMode { get; } =
-        new("Revive Type", (int)ReviveType.GroupSacrifice, typeof(ReviveType), ["Sacrifice", "Group Sacrifice", "Group Revive"]);
-
-    public ModdedNumberOption ReviveDuration { get; } =
-        new(TouLocale.Get("TouOptionAltruistReviveDuration", "Revive Duration"), 5f, 1f, 15f, 1f,
-            MiraNumberSuffixes.Seconds);
+        new("Revive Type", (int)ReviveType.GroupSacrifice, typeof(ReviveType),
+            ["Sacrifice", "Group Sacrifice", "Group Revive"]);
 
     public ModdedNumberOption ReviveRange { get; } =
         new(TouLocale.Get("TouOptionAltruistReviveRange", "Revive Range"), 0.25f, 0.05f, 1f, 0.05f,
@@ -28,6 +19,10 @@ public sealed class AltruistOptions : AbstractOptionGroup<AltruistRole>
         {
             Visible = () => OptionGroupSingleton<AltruistOptions>.Instance.ReviveMode != (int)ReviveType.Sacrifice
         };
+
+    public ModdedNumberOption ReviveDuration { get; } =
+        new(TouLocale.Get("TouOptionAltruistReviveDuration", "Revive Duration"), 5f, 1f, 15f, 1f,
+            MiraNumberSuffixes.Seconds);
 
     public ModdedNumberOption MaxRevives { get; } =
         new(TouLocale.Get("TouOptionAltruistMaxRevives", "Revive Uses"), 2f, 1f, 5f, 1f, MiraNumberSuffixes.None, "0");
@@ -37,6 +32,14 @@ public sealed class AltruistOptions : AbstractOptionGroup<AltruistRole>
 
     public ModdedToggleOption HideAtBeginningOfRevive { get; } =
         new(TouLocale.Get("TouOptionAltruistHideAtBeginningOfRevive", "Hide Bodies at Beginning Of Revive"), false);
+
+    public ModdedEnumOption KIllersAlertedAtStart { get; } =
+        new("Killers Alerted Before Revive", (int)InformedKillers.Nobody, typeof(InformedKillers),
+            ["Nobody", "Neutral Killers", "Impostors", "Neutrals and Impostors"]);
+
+    public ModdedEnumOption KIllersAlertedAtEnd { get; } =
+        new("Killers Alerted After Revive", (int)InformedKillers.NeutralsAndImpostors, typeof(InformedKillers),
+            ["Nobody", "Neutral Killers", "Impostors", "Neutrals and Impostors"]);
 }
 
 public enum InformedKillers
