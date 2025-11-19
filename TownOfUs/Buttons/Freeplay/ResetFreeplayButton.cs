57ee65492bcc970fb6e645ae3ae820b35ca8c506
Atony <75191600+AtonyGit@users.noreply.github.com>
2025-11-18 20:38:17 -0600

    Update ResetFreeplayButton.cs

--- TownOfUs/Buttons/Freeplay/ResetFreeplayButton.cs
+++ TownOfUs/Buttons/Freeplay/ResetFreeplayButton.cs
@@ -1,5 +1,11 @@
-﻿using MiraAPI.Hud;
+﻿using System.Collections;
+using AmongUs.GameOptions;
+using MiraAPI.Hud;
+using MiraAPI.Roles;
 using MiraAPI.Utilities.Assets;
+using Reactor.Utilities;
+using Reactor.Utilities.Extensions;
+using TownOfUs.Utilities;
 using UnityEngine;
 
 namespace TownOfUs.Buttons.Freeplay;
@@ -37,6 +43,50 @@ public sealed class ResetFreeplayButton : TownOfUsButton
         if (GameManager.Instance != null)
         {
             GameManager.Instance.ReviveEveryoneFreeplay();
+        }
+        Coroutines.Start(SetDummyData());
+    }
+
+    private static IEnumerator SetDummyData()
+    {
+        while (PlayerControl.LocalPlayer == null)
+        {
+            yield return null;
+        }
+
+        while (PlayerControl.LocalPlayer.Data == null)
+        {
+            yield return null;
+        }
+
+        while (PlayerControl.LocalPlayer.Data.Role == null)
+        {
+            yield return null;
+        }
+        yield return new WaitForSeconds(0.01f);
+
+        var roleList = RoleManager.Instance.AllRoles.ToArray()
+            .Where(role => !role.IsDead)
+            .Where(role => !role.IsImpostor())
+            .ToList();
+
+        foreach (var dummy in PlayerControl.AllPlayerControls.ToArray().Where(x => !x.AmOwner))
+        {
+            var random = roleList.Random();
+            if (random != null)
+            {
+                try
+                {
+                    var roleType = RoleId.Get(random.GetType());
+                    dummy.RpcChangeRole(roleType);
+                    roleList.Remove(random);
+                }
+                catch
+                {
+                    dummy.RpcChangeRole((ushort)RoleTypes.Crewmate);
+                }
+            }
+            yield return new WaitForSeconds(0.01f);
         }
     }
 }
