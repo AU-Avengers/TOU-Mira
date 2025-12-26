e0a6182e2f82ba481c8a733a90a6fbbf26fcf9e1
rewalo <170283204+rewalo@users.noreply.github.com>
2025-12-26 15:19:10 -0800

    Add reactor annoying warning supression

---
+++ TownOfUs/Patches/Misc/ReactorRpcWarningSuppressionPatch.cs
@@ -0,0 +1,40 @@
+using HarmonyLib;
+using BepInEx.Logging;
+
+namespace TownOfUs.Patches.Misc;
+
+/// <summary>
+///     Suppresses Reactor warnings about non-immediate RPCs being removed.
+///     These warnings spam the console but are harmless since Reactor now always sends immediately.
+/// </summary>
+[HarmonyPatch]
+public static class ReactorRpcWarningSuppressionPatch
+{
+    [HarmonyPatch(typeof(ManualLogSource), nameof(ManualLogSource.LogWarning), typeof(object))]
+    [HarmonyPrefix]
+    public static bool LogWarningPrefix(ManualLogSource __instance, object data)
+    {
+        if (__instance.SourceName == "Reactor" &&
+            data.ToString().Contains("Non-immediate RPCs were removed"))
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    [HarmonyPatch(typeof(ManualLogSource), nameof(ManualLogSource.Log), typeof(LogLevel), typeof(object))]
+    [HarmonyPrefix]
+    public static bool LogPrefix(ManualLogSource __instance, LogLevel level, object data)
+    {
+        if (level == LogLevel.Warning &&
+            __instance.SourceName == "Reactor" &&
+            data.ToString().Contains("Non-immediate RPCs were removed"))
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
+
