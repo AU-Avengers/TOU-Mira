d2933080615243faf05998d46c3195011882d5d6
Atony <75191600+AtonyGit@users.noreply.github.com>
2026-01-03 13:57:06 -0600

    Fix the character 不 being ignored in chat

--- TownOfUs/Patches/Roles/ChatControllerPatches.cs
+++ TownOfUs/Patches/Roles/ChatControllerPatches.cs
@@ -89,8 +89,8 @@ public static class ChatControllerPatches
         public static bool Prefix(char i, ref bool __result)
         {
             // Error($"checking if character {i.ToString()} (id {(int)i}) is allowed in chat");
-            if (i is '\b' or '\n' or '\r' or '>')
-            {   
+            if (i is '\b' or '\n' or '>')
+            {
                 return true;
             }
 
