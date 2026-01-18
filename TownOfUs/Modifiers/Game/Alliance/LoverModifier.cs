1fc7d2a91de281457190fca9992e6da501473eaf
Atony <75191600+AtonyGit@users.noreply.github.com>
2026-01-18 15:31:42 -0600

    Add IUnlovable interface for lover blacklisting

--- TownOfUs/Modifiers/Game/Alliance/LoverModifier.cs
+++ TownOfUs/Modifiers/Game/Alliance/LoverModifier.cs
@@ -10,6 +10,7 @@ using MiraAPI.Utilities.Assets;
 using Reactor.Networking.Attributes;
 using Reactor.Utilities;
 using TownOfUs.GameOver;
+using TownOfUs.Interfaces;
 using TownOfUs.Modifiers.Neutral;
 using TownOfUs.Options;
 using TownOfUs.Options.Modifiers;
@@ -76,7 +77,7 @@ public sealed class LoverModifier : AllianceGameModifier, IWikiDiscoverable, IAs
         foreach (var lover in PlayerControl.AllPlayerControls.ToArray().Where(x => x.HasModifier<LoverModifier>())
                      .ToList())
         {
-            lover.RemoveModifier<LoverModifier>();
+            lover.RpcRemoveModifier<LoverModifier>();
         }
 
         Random rnd = new();
@@ -90,7 +91,7 @@ public sealed class LoverModifier : AllianceGameModifier, IWikiDiscoverable, IAs
             var players = PlayerControl.AllPlayerControls.ToArray()
                 .Where(x => !x.HasDied() && !x.HasModifier<ExecutionerTargetModifier>() &&
                             !x.HasModifier<AllianceGameModifier>() &&
-                            x.Data.Role is not InquisitorRole && (loveOpt.NeutralLovers || !x.IsNeutral())).ToList();
+                            (x.Data.Role is not IUnlovable unlovable || !unlovable.IsUnlovable) && (loveOpt.NeutralLovers || !x.IsNeutral())).ToList();
             players.Shuffle();
 
             Random rndIndex1 = new();
