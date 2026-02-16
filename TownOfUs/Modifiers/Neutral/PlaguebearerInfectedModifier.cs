16c68cbe65c9396fdc3c5931c8ce8ef1b7bf6226
Atony <75191600+AtonyGit@users.noreply.github.com>
2026-02-15 20:25:59 -0600

    Adjust Plaguebearer code

--- TownOfUs/Modifiers/Neutral/PlaguebearerInfectedModifier.cs
+++ TownOfUs/Modifiers/Neutral/PlaguebearerInfectedModifier.cs
@@ -9,7 +9,6 @@ namespace TownOfUs.Modifiers.Neutral;
 
 public sealed class PlaguebearerInfectedModifier(byte plaguebearerId) : BaseModifier
 {
-    private readonly Color color = new(0.9f, 1f, 0.7f, 1f);
     public override string ModifierName => "Infected";
     public override bool HideOnUi => true;
 
@@ -22,18 +21,5 @@ public sealed class PlaguebearerInfectedModifier(byte plaguebearerId) : BaseModi
         var pb = PlayerControl.AllPlayerControls.ToArray().FirstOrDefault(x => x.PlayerId == PlagueBearerId);
         var touAbilityEvent = new TouAbilityEvent(AbilityType.PlaguebearerInfect, pb!, Player);
         MiraEventManager.InvokeEvent(touAbilityEvent);
-    }
-
-    public override void FixedUpdate()
-    {
-        if (PlayerControl.LocalPlayer.IsRole<PlaguebearerRole>() && Player != PlayerControl.LocalPlayer)
-        {
-            Player?.cosmetics.SetOutline(true, new Il2CppSystem.Nullable<Color>(color));
-        }
-    }
-
-    public override void OnDeactivate()
-    {
-        Player.cosmetics.SetOutline(false, new Il2CppSystem.Nullable<Color>(color));
     }
 }
