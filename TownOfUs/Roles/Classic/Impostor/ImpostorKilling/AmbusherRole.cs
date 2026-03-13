55f7850c6712bce017968d0950b7ff4da0087c0b
Atony <75191600+AtonyGit@users.noreply.github.com>
2026-03-12 20:41:40 -0500

    Fix issues with Ambusher & Invest prints

--- TownOfUs/Roles/Classic/Impostor/ImpostorKilling/AmbusherRole.cs
+++ TownOfUs/Roles/Classic/Impostor/ImpostorKilling/AmbusherRole.cs
@@ -20,6 +20,7 @@ using TownOfUs.Buttons.Impostor;
 using TownOfUs.Events;
 using TownOfUs.Modifiers;
 using TownOfUs.Modifiers.Game.Universal;
+using TownOfUs.Modifiers.Impostor;
 using TownOfUs.Modules.Anims;
 using TownOfUs.Options.Roles.Impostor;
 using TownOfUs.Roles.Crewmate;
@@ -113,26 +114,6 @@ public sealed class AmbusherRole(IntPtr cppPtr)
         TouRoleUtils.ClearTaskHeader(Player);
 
         Clear();
-        var ambushAnim = GameObject.Find($"{Player.Data.PlayerName} Ambush Animation");
-
-        if (ambushAnim != null)
-        {
-            ambushAnim.gameObject.Destroy();
-
-            Player.Visible = true;
-
-            foreach (var shield in Player.GetModifiers<BaseShieldModifier>())
-            {
-                shield.IsVisible = true;
-                shield.SetVisible();
-            }
-
-            if (Player.HasModifier<FirstDeadShield>())
-            {
-                Player.GetModifier<FirstDeadShield>()!.IsVisible = true;
-                Player.GetModifier<FirstDeadShield>()!.SetVisible();
-            }
-        }
     }
 
     public void Clear()
@@ -174,7 +155,8 @@ public sealed class AmbusherRole(IntPtr cppPtr)
         if (murderResultFlags2.HasFlag(MurderResultFlags.Succeeded) &&
             murderResultFlags2.HasFlag(MurderResultFlags.DecisionByHost))
         {
-            DeathHandlerModifier.UpdateDeathHandlerImmediate(target, TouLocale.Get("DiedToAmbusherAmbush"), DeathEventHandlers.CurrentRound,
+            DeathHandlerModifier.UpdateDeathHandlerImmediate(target, TouLocale.Get("DiedToAmbusherAmbush"),
+                DeathEventHandlers.CurrentRound,
                 DeathHandlerOverride.SetTrue,
                 TouLocale.GetParsed("DiedByStringBasic").Replace("<player>", ambusher.Data.PlayerName),
                 lockInfo: DeathHandlerOverride.SetTrue);
@@ -186,184 +168,6 @@ public sealed class AmbusherRole(IntPtr cppPtr)
             true,
             true,
             false);
-        Coroutines.Start(CoSetBodyReportable(ambusher, target));
-    }
-
-    private static IEnumerator CoSetBodyReportable(PlayerControl ambusher, PlayerControl target)
-    {
-        var ogPos = ambusher.transform.position;
-        yield return new WaitForSeconds(0.01f);
-        if (!target.HasDied() || MeetingHud.Instance || ambusher.HasDied())
-        {
-            yield break;
-        }
-
-        var bodyId = target.PlayerId;
-        var waitDelegate =
-            DelegateSupport.ConvertDelegate<Il2CppSystem.Func<bool>>(() => Helpers.GetBodyById(bodyId) != null);
-        yield return new WaitUntil(waitDelegate);
-        var body = Helpers.GetBodyById(bodyId);
-
-        if (body != null)
-        {
-            DeathHandlerModifier.UpdateDeathHandlerImmediate(target, TouLocale.Get("DiedToAmbusherAmbush"),
-                DeathEventHandlers.CurrentRound,
-                DeathHandlerOverride.SetTrue,
-                TouLocale.GetParsed("DiedByStringBasic").Replace("<player>", ambusher.Data.PlayerName),
-                lockInfo: DeathHandlerOverride.SetTrue);
-
-            var bodyPos = body.transform.position;
-            if (MeetingHud.Instance == null && ambusher.AmOwner)
-            {
-                ambusher.moveable = false;
-                ambusher.MyPhysics.ResetMoveState();
-                ambusher.NetTransform.SetPaused(true);
-                bodyPos.y += 0.175f;
-                bodyPos.z = bodyPos.y / 1000f;
-                ambusher.RpcSetPos(bodyPos);
-            }
-
-            // Hide real player
-            ambusher.Visible = false;
-            foreach (var shield in ambusher.GetModifiers<BaseShieldModifier>())
-            {
-                shield.IsVisible = false;
-                shield.SetVisible();
-            }
-
-            if (ambusher.HasModifier<FirstDeadShield>())
-            {
-                ambusher.GetModifier<FirstDeadShield>()!.IsVisible = false;
-                ambusher.GetModifier<FirstDeadShield>()!.SetVisible();
-            }
-
-            var bodySprite = body.transform.GetChild(1).gameObject;
-            var ambushAnim = AnimStore.SpawnFliplessAnimBody(ambusher, TouAssets.AmbushPrefab.LoadAsset());
-            ambushAnim.name = $"{ambusher.Data.PlayerName} Ambush Animation";
-            ambushAnim.SetActive(false);
-
-            yield return new WaitForSeconds(1.3f);
-
-            if (!target.HasDied() || MeetingHud.Instance || ambusher.HasDied())
-            {
-                ambushAnim.gameObject.Destroy();
-                ambusher.Visible = true;
-
-                foreach (var shield in ambusher.GetModifiers<BaseShieldModifier>())
-                {
-                    shield.IsVisible = true;
-                    shield.SetVisible();
-                }
-
-                if (ambusher.HasModifier<FirstDeadShield>())
-                {
-                    ambusher.GetModifier<FirstDeadShield>()!.IsVisible = true;
-                    ambusher.GetModifier<FirstDeadShield>()!.SetVisible();
-                }
-
-                if (!ambusher.AmOwner)
-                {
-                    yield break;
-                }
-
-                ambusher.moveable = true;
-                ambusher.NetTransform.SetPaused(false);
-                yield break;
-            }
-
-            ambushAnim.SetActive(true);
-            var spriteAnim = ambushAnim.GetComponent<SpriteAnim>();
-            var animationRend = ambushAnim.transform.GetChild(0).GetComponent<SpriteRenderer>();
-            animationRend.material = bodySprite.GetComponent<SpriteRenderer>().material;
-            body.gameObject.transform.position = new Vector3(bodyPos.x, bodyPos.y, bodyPos.z + 1000f);
-
-            if (ambusher.HasModifier<GiantModifier>())
-            {
-                ambushAnim.transform.localScale *= 0.7f;
-            }
-            else if (ambusher.HasModifier<MiniModifier>())
-            {
-                ambushAnim.transform.localScale /= 0.7f;
-            }
-
-            if (target.HasModifier<MiniModifier>())
-            {
-                ambushAnim.transform.localScale *= 0.7f;
-            }
-            else if (target.HasModifier<GiantModifier>())
-            {
-                ambushAnim.transform.localScale /= 0.7f;
-            }
-
-            yield return new WaitForSeconds(spriteAnim.m_defaultAnim.length);
-
-            if (!target.HasDied() || MeetingHud.Instance || ambusher.HasDied())
-            {
-                ambushAnim.gameObject.Destroy();
-                ambusher.Visible = true;
-
-                foreach (var shield in ambusher.GetModifiers<BaseShieldModifier>())
-                {
-                    shield.IsVisible = true;
-                    shield.SetVisible();
-                }
-
-                if (ambusher.HasModifier<FirstDeadShield>())
-                {
-                    ambusher.GetModifier<FirstDeadShield>()!.IsVisible = true;
-                    ambusher.GetModifier<FirstDeadShield>()!.SetVisible();
-                }
-
-                if (!MeetingHud.Instance && !ambusher.HasDied())
-                {
-                    ambusher.transform.position = ogPos;
-                    ambusher.NetTransform.SnapTo(ogPos);
-                }
-
-                if (!ambusher.AmOwner)
-                {
-                    yield break;
-                }
-
-                ambusher.moveable = true;
-                ambusher.NetTransform.SetPaused(false);
-                yield break;
-            }
-
-            ambushAnim.gameObject.Destroy();
-
-            if (MeetingHud.Instance == null && target.HasDied())
-            {
-                if (ambusher.AmOwner)
-                {
-                    ambusher.RpcSetPos(ogPos);
-                }
-                var targetPos = ogPos + new Vector3(-0.05f, 0.175f, 0f);
-                targetPos.z = targetPos.y / 1000f;
-                body.transform.position = (ambusher.Collider.bounds.center - targetPos) + targetPos;
-            }
-
-            ambusher.Visible = true;
-
-            foreach (var shield in ambusher.GetModifiers<BaseShieldModifier>())
-            {
-                shield.IsVisible = true;
-                shield.SetVisible();
-            }
-
-            if (ambusher.HasModifier<FirstDeadShield>())
-            {
-                ambusher.GetModifier<FirstDeadShield>()!.IsVisible = true;
-                ambusher.GetModifier<FirstDeadShield>()!.SetVisible();
-            }
-
-            if (!ambusher.AmOwner)
-            {
-                yield break;
-            }
-
-            ambusher.moveable = true;
-            ambusher.NetTransform.SetPaused(false);
-        }
+        ambusher.AddModifier<AmbusherConcealedModifier>(target);
     }
 }
