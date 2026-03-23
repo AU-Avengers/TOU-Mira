56669b854c7fe31b593347b174fe422e8e01145a
Atony <75191600+AtonyGit@users.noreply.github.com>
2026-03-23 17:44:23 -0500

    Show game region at all times

---
+++ TownOfUs/Patches/Misc/PingTrackerPatch.cs
@@ -0,0 +1,16 @@
+using HarmonyLib;
+using TownOfUs.Utilities;
+
+namespace TownOfUs.Patches.Misc;
+
+[HarmonyPatch(typeof(PingTracker), nameof(PingTracker.Update))]
+internal static class PingTrackerPatch
+{
+    [HarmonyPostfix]
+    [HarmonyPriority(Priority.First)]
+    public static void Postfix(PingTracker __instance)
+    {
+        var extraText = $"<align=center><size=60%><space=3em>Region: <color=#C96DFF>{MiscUtils.GetRegionName()}</color></size></align>";
+        __instance.text.text += $"\r\n{extraText}";
+    }
+}
