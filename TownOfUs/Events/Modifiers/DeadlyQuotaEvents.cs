6be97e32d4702c2d202fd8379ea167869f0a82df
Atony <75191600+AtonyGit@users.noreply.github.com>
2026-03-27 21:03:55 -0500

    Remove warning

--- TownOfUs/Events/Modifiers/DeadlyQuotaEvents.cs
+++ TownOfUs/Events/Modifiers/DeadlyQuotaEvents.cs
@@ -7,7 +7,6 @@ using MiraAPI.GameOptions;
 using MiraAPI.Hud;
 using TownOfUs.Buttons;
 using TownOfUs.Options;
-using TownOfUs.Utilities;
 using UnityEngine;
 using TownOfUs.Options.Modifiers.Impostor;
 using MiraAPI.Utilities;
