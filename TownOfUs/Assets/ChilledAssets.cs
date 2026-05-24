9b96fbe9a4b7161feebba685cbcae51a2f04c4c5
Atony <75191600+AtonyGit@users.noreply.github.com>
2026-05-23 21:28:13 -0500

    Add chilled asset bundle

---
+++ TownOfUs/Assets/ChilledAssets.cs
@@ -0,0 +1,9 @@
+using Reactor.Utilities;
+using UnityEngine;
+
+namespace TownOfUs.Assets;
+
+public static class ChilledAssets
+{
+    public static readonly AssetBundle ChilledBundle = AssetBundleManager.Load("chilled-assets");
+}
