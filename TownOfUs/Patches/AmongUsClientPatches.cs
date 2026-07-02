4abf7ce777196be6d3947901bc729ba5e5da12df
Atony <75191600+AtonyGit@users.noreply.github.com>
2026-07-02 15:28:47 -0500

    Disable Medium Spirit due to fatal error

--- TownOfUs/Patches/AmongUsClientPatches.cs
+++ TownOfUs/Patches/AmongUsClientPatches.cs
@@ -19,13 +19,15 @@ public static class AmongUsClientPatches
         // This allows the custom door types to update properly
         Warning("Added TOU Mira System Types!");
         SystemTypeHelpers.AllTypes.AddRangeToArray([HexBombSabotageSystem.SystemType, SkeldDoorsSystemType.SystemType, ManualDoorsSystemType.SystemType]);
+        Error("TOU Mira Spawnables are temporarily disabled. Medium will not work.");
 
+        /*
         Warning("Added TOU Mira Spawnables.");
         var medSpirit = TouAssets.MediumSpirit.LoadAsset().GetComponent<MedSpiritObject>();
         medSpirit.SpawnId = (uint)__instance.SpawnableObjects.Length;
         __instance.SpawnableObjects =
             __instance.SpawnableObjects.AddRangeToArray([__instance.SpawnableObjects[0]]); // dummy value
 
-        __instance.NonAddressableSpawnableObjects.AddRangeToArray([medSpirit]);
+        __instance.NonAddressableSpawnableObjects.AddRangeToArray([medSpirit]);*/
     }
 }
