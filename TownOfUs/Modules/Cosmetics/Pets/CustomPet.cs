75a6fc42a3d44b5d260bd63e32812fbbc8796824
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2026-07-09 22:49:00 -0600

    Changed to record

--- TownOfUs/Modules/Cosmetics/Pets/CustomPet.cs
+++ TownOfUs/Modules/Cosmetics/Pets/CustomPet.cs
@@ -2,17 +2,10 @@
 
 namespace TownOfUs.Modules.Cosmetics.Pets;
 
-public class CustomPet(
-    string id,
-    PetData petData,
-    PetBehaviour petBehaviour,
-    PreviewViewData previewData,
-    GameObject obj
-        )
-{
-    public string Id { get; } = id;
-    public PetData PetData { get; } = petData;
-    public PetBehaviour PetBehaviour { get; } = petBehaviour;
-    public GameObject Obj { get; } = obj;
-    public PreviewViewData PreviewData { get; } = previewData;
-}
+public record CustomPet(
+    string Id,
+    PetData PetData,
+    PetBehaviour PetBehaviour,
+    PreviewViewData PreviewData,
+    GameObject Obj
+);
