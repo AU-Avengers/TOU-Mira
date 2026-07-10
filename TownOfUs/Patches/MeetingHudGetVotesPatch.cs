3b6741c318206fcad3a713ef7e3e9d066715673f
Atony <75191600+AtonyGit@users.noreply.github.com>
2026-07-09 22:55:09 -0500

    Remove useless populate results patch

--- TownOfUs/Patches/MeetingHudGetVotesPatch.cs
+++ TownOfUs/Patches/MeetingHudGetVotesPatch.cs
@@ -1,7 +1,5 @@
 ﻿using HarmonyLib;
 using Il2CppInterop.Runtime.InteropTypes.Arrays;
-using MiraAPI.Roles;
-using TownOfUs.Roles.Crewmate;
 
 namespace TownOfUs.Patches;
 
@@ -34,14 +32,5 @@ public static class MeetingHudGetVotesPatch
     public static void StartPostfix()
     {
         States = [];
-    }
-
-    [HarmonyPrefix]
-    [HarmonyPatch(nameof(MeetingHud.PopulateResults))]
-    public static void PopulatePrefix(MeetingHud __instance, Il2CppStructArray<MeetingHud.VoterState> states)
-    {
-        var swapper = CustomRoleUtils.GetActiveRolesOfType<SwapperRole>().FirstOrDefault(swapper => !swapper.Player.HasDied() && swapper.Swap1 && swapper.Swap2);
-        if (swapper == null)
-        {}
     }
 }
