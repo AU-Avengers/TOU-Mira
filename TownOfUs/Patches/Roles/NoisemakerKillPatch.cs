95d5e4b951ec6374e2f2bd8ee39fcdfd23a8e334
Atony <75191600+AtonyGit@users.noreply.github.com>
2026-07-17 16:32:10 -0500

    Account for viper / rotting + patch NM role check

--- TownOfUs/Patches/Roles/NoisemakerKillPatch.cs
+++ TownOfUs/Patches/Roles/NoisemakerKillPatch.cs
@@ -5,15 +5,11 @@ namespace TownOfUs.Patches.Roles;
 [HarmonyPatch(typeof(NoisemakerRole))]
 public static class NoisemakerKillPatch
 {
+    // we run this code elsewhere!
     [HarmonyPrefix]
     [HarmonyPatch(nameof(NoisemakerRole.NotifyOfDeath))]
     public static bool Prefix(NoisemakerRole __instance)
     {
-        var scBody = MiscUtils.GetFakePlayer(__instance.Player);
-        if (scBody != null)
-        {
-            return false;
-        }
-        return true;
+        return false;
     }
 }
