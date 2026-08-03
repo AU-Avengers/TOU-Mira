5846cd0c6c041555ccfb0e765244b5aa362f69c1
Atony <75191600+AtonyGit@users.noreply.github.com>
2026-08-02 23:59:47 -0500

    Update more option titles

--- TownOfUs/Options/Maps/BetterMiraHqOptions.cs
+++ TownOfUs/Options/Maps/BetterMiraHqOptions.cs
@@ -9,7 +9,7 @@ namespace TownOfUs.Options.Maps;
 public sealed class BetterMiraHqOptions : AbstractOptionGroup
 {
     public override MenuCategory ParentMenu => MenuCategory.CustomOne;
-    public override string GroupName => "Better Mira HQ";
+    public override string GroupName => TouLocale.Get("TouOptionTitleBetterMira");
     public override uint GroupPriority => 4;
     public override Color GroupColor => new Color32(255, 128, 100, 255);
     public override OptionNotifConfiguration Configuration => new(
