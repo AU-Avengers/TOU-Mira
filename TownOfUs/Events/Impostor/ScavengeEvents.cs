8b4e977aefd44540333442a2f468154d368b178f
neilmjain <69025896+neilmjain@users.noreply.github.com>
2026-09-18 18:26:31 -0400

    Add Visible Timer to Scavenger Kill Button

--- TownOfUs/Events/Impostor/ScavengeEvents.cs
+++ TownOfUs/Events/Impostor/ScavengeEvents.cs
@@ -10,7 +10,13 @@ public static class ScavengerEvents
     public static void AfterMurderEventHandler(AfterMurderEvent @event)
     {
         var source = @event.Source;
-        if (!source.AmOwner || source.Data.Role is not ScavengerRole scavenger)
+        if (!source || !source.AmOwner || !source.IsRole<ScavengerRole>())
+        {
+            return;
+        }
+
+        var scavenger = source.GetRole<ScavengerRole>();
+        if (scavenger == null)
         {
             return;
         }
